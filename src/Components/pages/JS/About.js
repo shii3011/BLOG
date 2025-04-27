395e1262909ed522efa4e18c792530a10bd8480e
“shii3011”~ <yasutaka3011@gmail.com>
2025-04-27 15:05:40 +0900

    Deploy React App to GitHub Pages

---
+++ src/Components/pages/JS/About.js
@@ -0,0 +1,14 @@
+// pages/About.js
+import React from 'react';
+import './../CSS/About.css';
+
+function About() {
+  return (
+    <div className = "About">
+      <h2>ABOUT</h2>
+      <p>こちらはABOUTページの内容です。</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Components/pages/JS/Contact.js
@@ -0,0 +1,14 @@
+// pages/Contact.js
+import React from 'react';
+import './../CSS/Contact.css';
+
+function Contact() {
+  return (
+    <div className = "Contact">
+      <h2>Contact</h2>
+      <p>こちらはContactページの内容です。</p>
+    </div>
+  );
+}
+
+export default Contact;
